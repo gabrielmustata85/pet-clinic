01cf18fe17200cc657654aa07ff948a104a08f5e
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-12 16:40:38 +0300

    adding pojo's

---
+++ src/main/java/com/example/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.example.petclinic.model;
+
+public class PetType {
+
+    private String petName;
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+}
