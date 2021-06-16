907e86bd325eb0c8d4e8b6472c95aece68d82dea
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-16 19:36:16 +0300

    owner list page and owner controller added

---
+++ pet-clinic-web/src/main/java/pet/clinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package pet.clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping("/owners")
+    public String listOwners(){
+
+        return "/owners/listOwner";
+
+    }
+}
