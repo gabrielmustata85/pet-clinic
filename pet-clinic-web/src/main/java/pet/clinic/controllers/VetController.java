6c20edf20f4e6b6d4a5d1118ea0e484e00a2c98b
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-20 07:39:20 +0300

    list owners

--- pet-clinic-web/src/main/java/pet/clinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pet/clinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package pet.clinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pet.clinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll() );
 
         return "/vets/listVets";
     }
