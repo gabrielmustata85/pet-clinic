e82688bbd9af663516b8ac5c4ab516c456bc2767
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-26 17:22:23 +0300

    adding JPA entities

--- pet-clinic-data/src/main/java/pet/clinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pet/clinic/model/Owner.java
@@ -1,13 +1,24 @@
 package pet.clinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name="address")
     private String address;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @Column(name = "city")
     private String city;
+
+    //fyi: (cascade) if we delete an owner, the pet will be deleted as well
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
