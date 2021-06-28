ab0cf3bc71ed33e2a46c9f039e694327d9618df8
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-28 19:42:59 +0300

    adding lombok

--- pet-clinic-data/src/main/java/pet/clinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pet/clinic/model/PetType.java
@@ -1,9 +1,16 @@
 package pet.clinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
@@ -11,11 +18,5 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String petName;
 
-    public String getPetName() {
-        return petName;
-    }
 
-    public void setPetName(String petName) {
-        this.petName = petName;
-    }
 }
--- pet-clinic-data/src/main/java/pet/clinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pet/clinic/model/Visit.java
@@ -1,10 +1,16 @@
 package pet.clinic.model;
 
+import lombok.*;
 import lombok.extern.apachecommons.CommonsLog;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -19,27 +25,4 @@ public class Visit extends BaseEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
